80568b68f01767edf579694564c49e0447feac20
Beto <beto@2sidesof1.com>
2025-01-18 16:59:51 -0300

    feat: updates

--- betofigueiredo/vite.config.ts
+++ betofigueiredo/vite.config.ts
@@ -1,8 +1,16 @@
-import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { VitePluginRadar } from "vite-plugin-radar";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react()],
+  plugins: [
+    TanStackRouterVite(),
+    react(),
+    VitePluginRadar({
+      // Google Analytics tag injection
+      analytics: { id: "G-XW4N8TTZTK" },
+    }),
+  ],
 });
